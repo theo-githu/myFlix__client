e615901f8161b6374063c2a1091e78131530b059
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-09-02 18:11:37 +0100

    Start

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,15 @@
+
+
+export const MovieCard = ({ movie, onMovieClick }) => {
+
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
+};
+
